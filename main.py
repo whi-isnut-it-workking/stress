c25848666c705391d696def2cd233d0ab43490f3
ikaman3 <yjhama1532@gmail.com>
2023-11-19 19:13:41 +0900

    Fix : CORS 에러 코드 추가

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
-from apis import board_router, comment_router
+from apis import board_router, comment_router, utils_router
 
 app = FastAPI()
 
+app.middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost",
+        "http://125.143.133.111",
+        "http://3.34.107.69",
+        "http://121.128.211.44",
+        "http://1.240.130.87"
+    ],
+    allow_credentials=True,
+    allow_methods=["GET, POST, PUT, PATCH, DELETE"],
+    allow_headers=["*"]
+)
+
 app.include_router(router=board_router.router)
 app.include_router(router=comment_router.router)
+app.include_router(router=utils_router.router)
