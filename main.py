68f3b5ca5ab4e5533401afd1c02e637d67cb7f47
ikaman3 <yjhama1532@gmail.com>
2023-11-25 14:34:53 +0900

    Fix : CORS 미들웨어 오타 수정

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ app.add_middleware(
         "*"
     ],
     allow_credentials=True,
-    allow_methods=["GET, POST, PUT, PATCH, DELETE"],
+    allow_methods=["GET", "POST", "PUT", "PATCH", "DELETE"],
     allow_headers=["*"]
 )
 
